f465fd555d7b3d6b115cf53b92cc3b8c5b42b83e
Spacepineapple <neal.t.reeves@gmail.com>
2023-03-15 18:38:49 +0000

    Create components folder and initialise footer skeleton

---
+++ src/components/footer.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            <div class="header-upper">
+                <div>
+                    <h4>Just Buy</h4>
+                    <p>eCommerce Made Easy</p>
+                </div>
+                <div>
+                    <ul>
+                        <li>Products</li>
+                        <li>Category One</li>
+                        <li>Category Two</li>
+                        <li>Category Three</li>
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                        <li>About Us</li>
+                        <li>Sitemap</li>
+                        <li>FAQ</li>
+                        <li>Terms and Conditions</li>
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                        <li>Customer Service</li>
+                        <li>Contact Us</li>
+                        <li>Order Tracking</li>
+                        <li>Shipping and Returns</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="header-lower">
+                <div>
+                    <p>&copy; Just Buy</p>
+                    <p>We accept all major credit cards</p>
+                    <p>Back to Top</p>
+                </div>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
+
