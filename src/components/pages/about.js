f9d287c8ecaae8a4e447b51e51a6ab23bb8f4a09
Spacepineapple <neal.t.reeves@gmail.com>
2023-03-15 19:13:14 +0000

    Initialise about page

---
+++ src/components/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AboutUs() {
+    return (
+        <div class="container">
+            <h2>About Us</h2>
+            <p>Welcome and thank you for choosing JustBuy.</p>
+        </div>
+    )
+}
+
+export default AboutUs;
