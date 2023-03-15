388ac92b3efe896247da713fee5ffd4ce02f988b
Spacepineapple <neal.t.reeves@gmail.com>
2023-03-15 19:33:37 +0000

    Initialise Terms and Conditions page

---
+++ src/components/pages/Terms.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Terms() {
+    return (
+        <div class="container">
+            <h2>Terms and Conditions</h2>
+            <p>Just Buy is a registered trademark. All text including but not limited to prices and delivery timeframes are indicative and do not represent a guarantee. All products, images, reviews and prices are completely fake.</p>
+        </div>
+    )
+}
+
+export default Terms;
