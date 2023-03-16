31ae5cb6e297f2d7f1e2e9d3c7471a4f38e2c3ec
Spacepineapple <neal.t.reeves@gmail.com>
2023-03-16 20:41:42 +0000

    Create Returns page with basic Jumbotron structure

---
+++ src/components/pages/Returns.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Shipping() {
+    return(
+        <div className="p-5 mb-4 bg-light rounded-3">
+        <div className="container-fluid py-5">
+            <h2 className="display-5 fw-bold">Returns Policy</h2>
+            <p className="col-md-8 fs-4">To find out more about our returns policy, click the button below</p>
+            <button className="btn btn-primary btn-lg">View Policy</button>
+        </div>
+    </div>
+    )
+}
+
+export default Shipping;
