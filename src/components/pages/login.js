84a9266fd66e9b680457208e1339535a488dffea
Julien Claivaz <julienclaivaz@192.168.1.101>
2023-03-17 18:04:39 +0000

    orgonised login funcition

--- src/components/pages/login.js
+++ src/components/pages/login.js
@@ -1,32 +1,31 @@
 import React from "react";
 
-function LoginDropMenu(){
+export default function LoginDropMenu(){
 
     return(
-        <div className="dropdown-menu">
-  <form className="px-4 py-3">
-    <div className="form-group">
-      <label for="exampleDropdownFormEmail1">Email address</label>
-      <input type="email" className="form-control" id="exampleDropdownFormEmail1" placeholder="email@example.com"/>
-    </div>
-    <div className="form-group">
-      <label for="exampleDropdownFormPassword1">Password</label>
-      <input type="password" className="form-control" id="exampleDropdownFormPassword1" placeholder="Password"/>
-    </div>
-    <div className="form-check">
-      <input type="checkbox" className="form-check-input" id="dropdownCheck"/>
-      <label className="form-check-label" for="dropdownCheck">
+<div className="dropdown-menu">
+    <form className="px-4 py-3">
+        <div className="form-group">
+            <label for="exampleDropdownFormEmail1">Email address</label>
+            <input type="email" className="form-control" id="exampleDropdownFormEmail1" placeholder="email@example.com"/>
+        </div>
+        <div className="form-group">
+            <label for="exampleDropdownFormPassword1">Password</label>
+            <input type="password" className="form-control" id="exampleDropdownFormPassword1" placeholder="Password"/>
+        </div>
+        <div className="form-check">
+            <input type="checkbox" className="form-check-input" id="dropdownCheck"/>
+            <label className="form-check-label" for="dropdownCheck">
         Remember me
-      </label>
-    </div>
-    <button type="submit" className="btn btn-primary">Sign in</button>
-  </form>
-  <div className="dropdown-divider"></div>
-  <a className="dropdown-item" href="#">Sign up</a>
+            </label>
+        </div>
+        <button type="submit" className="btn btn-primary">Sign in</button>
+    </form>
+    <div className="dropdown-divider"></div>
+    <a className="dropdown-item" href="#">Sign up</a>
 </div>
-
     )
-
 }
 
-export default LoginDropMenu
+
+
