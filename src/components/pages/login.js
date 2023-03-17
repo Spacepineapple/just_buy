619c08cb2ad88ac01c1374adc8a24fd50f517efd
Julien Claivaz <julienclaivaz@192.168.1.101>
2023-03-17 18:52:27 +0000

    added modal for sign up

--- src/components/pages/login.js
+++ src/components/pages/login.js
@@ -22,10 +22,28 @@ export default function LoginDropMenu(){
         <button type="submit" className="btn btn-primary">Sign in</button>
     </form>
     <div className="dropdown-divider"></div>
-    <a className="dropdown-item" href="#">Sign up</a>
+    <a className="dropdown-item" data-bs-toggle="modal" data-target="#signUp" href="#">Sign up</a>
+    <div className="modal fade" id="signUp" tabindex="-1" role="dialog" aria-labelledby="signUpLabel" aria-hidden="true">
+  <div className="modal-dialog" role="document">
+    <div className="modal-content">
+      <div className="modal-header">
+        <h5 className="modal-title" id="signUpLabel">Modal title</h5>
+        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div className="modal-body">
+
+      </div>
+      <div className="modal-footer">
+        <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" className="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
 </div>
     )
 }
-
 
 
