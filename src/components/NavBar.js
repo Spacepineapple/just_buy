ad7d8c3740b66d797f7ce449fd208061f3ea2d86
Julien Claivaz <julienclaivaz@192.168.1.101>
2023-03-17 17:39:37 +0000

    added login component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Login from "./pages/login"
 
 
 function NavBar(){
@@ -41,11 +42,12 @@ function NavBar(){
                     </svg><sup><span class="badge rounded-pill badge-notification bg-danger">1</span></sup>
                 </a>
             </li>
-            <li className="nav-item">
-                <a className="nav-link" href="#"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+            <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
                     <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
                     </svg>Login/Register
                 </a>
+                <Login></Login>
             </li>
         </ul>
     </div>
