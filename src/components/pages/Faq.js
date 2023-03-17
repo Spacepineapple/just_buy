e7e567a326bb2b4e2a12065b544e3d7d087c389e
Spacepineapple <neal.t.reeves@gmail.com>
2023-03-17 19:47:02 +0000

    Return FAQ to pages folder

---
+++ src/components/pages/Faq.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function FAQ() {
+    return (
+        <div className="container">
+            <h2>Frequently Asked Questions</h2>
+            <p>We love answering your questions, but to save us all some time, we compiled some of the most common things we're asked and our answers below. Still got a question? Find out more about how to contact us on our contact page!</p>
+            <h3>What is Just Buy?</h3>
+            <h3>What do you sell?</h3>
+            <h3>How is Just Buy different from other shopping services?</h3>
+            <h3>What delivery options do you offer?</h3>
+            <h3>Can I return items?</h3>
+        </div>
+    )
+}
+
+export default FAQ;
