4c120fb64ca765e2a7d28fbc2a3e4fd9677fd4a7
Julien Claivaz <julienclaivaz@192.168.1.101>
2023-03-17 19:28:57 +0000

    fixed modal issue

--- src/components/pages/login.js
+++ src/components/pages/login.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function LoginDropMenu(){
 
-    return(
+    return(<>
 <div className="dropdown-menu">
     <form className="px-4 py-3">
         <div className="form-group">
@@ -23,26 +23,27 @@ export default function LoginDropMenu(){
     </form>
     <div className="dropdown-divider"></div>
     <a className="dropdown-item" data-bs-toggle="modal" data-bs-target="#signUp" href="#">Sign up</a>
-    <div className="modal fade" id="signUp" tabindex="-1" role="dialog" aria-labelledby="signUpLabel" aria-hidden="true">
-  <div className="modal-dialog" role="document">
-    <div className="modal-content">
-      <div className="modal-header">
-        <h5 className="modal-title" id="signUpLabel">Modal title</h5>
-        <button type="button" className="close" data-bs-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div className="modal-body">
+</div>
 
-      </div>
-      <div className="modal-footer">
-        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" className="btn btn-primary">Save changes</button>
-      </div>
+<div className="modal fade" id="signUp" tabindex="-1" role="dialog" aria-labelledby="signUpLabel" aria-hidden="true">
+    <div className="modal-dialog" role="document">
+        <div className="modal-content">
+            <div className="modal-header">
+                <h5 className="modal-title" id="signUpLabel">Modal title</h5>
+                <button type="button" className="close" data-bs-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div className="modal-body">
+            </div>
+            <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" className="btn btn-primary">Save changes</button>
+            </div>
+        </div>
     </div>
-  </div>
 </div>
-</div>
+</>
     )
 }
 
