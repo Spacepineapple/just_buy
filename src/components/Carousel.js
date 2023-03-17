5d3182171540758a4b1910a1fe018361636b3c7c
Osman Dumbuya <dumbuyaosman@icloud.com>
2023-03-17 18:12:36 +0000

    Add featured header object for the carousel items

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,3 +1,29 @@
+import CarouselItem from "./CarouselItem";
+
+const carouselItem = [
+  {
+    id: 1,
+    name: "Content 1",
+    image:
+      "https://images.unsplash.com/photo-1513885535751-8b9238bd345a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    description: "Find the perfect gift this mothers day.",
+  },
+  {
+    id: 2,
+    name: "Content 2",
+    image:
+      "https://images.unsplash.com/photo-1441986300917-64674bd600d8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    description: "We have thousands of products on sale.",
+  },
+  {
+    id: 3,
+    name: "Content 3",
+    image:
+      "https://images.unsplash.com/photo-1615655406736-b37c4fabf923?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    description: "Explore our range of products.",
+  },
+];
+
 export default function Carousel() {
   return (
     <div id="myCarousel" className="carousel slide" data-bs-ride="carousel">
@@ -29,49 +55,30 @@ export default function Carousel() {
         <div className="carousel-item active">
           {/* Todo - Add link */}
           <a href="#">
-            <img
-              src="https://images.unsplash.com/photo-1513885535751-8b9238bd345a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-              aria-hidden="true"
-              preserveAspectRatio="xMidYMid slice"
-              focusable="false"
-              alt=""
-            />
-            <div className="container">
+            <img src={carouselItem[0].image} aria-hidden="true" alt="" />
+            <div>
               <div className="carousel-caption text-start">
-                <h1>Find the perfect gift this mothers day.</h1>
+                <h1>{carouselItem[0].description}</h1>
               </div>
             </div>
           </a>
         </div>
         <div className="carousel-item">
           <a href="#">
-            <img
-              src="https://images.unsplash.com/photo-1441986300917-64674bd600d8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-              aria-hidden="true"
-              preserveAspectRatio="xMidYMid slice"
-              focusable="false"
-              alt=""
-            />
-            <div className="container">
+            <img src={carouselItem[1].image} aria-hidden="true" alt="" />
+            <div>
               <div className="carousel-caption">
-                <h1>We have thousands of products on sale.</h1>
-                <p></p>
+                <h1>{carouselItem[1].description}</h1>
               </div>
             </div>
           </a>
         </div>
         <div className="carousel-item">
           <a href="#">
-            <img
-              src="https://images.unsplash.com/photo-1615655406736-b37c4fabf923?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-              aria-hidden="true"
-              preserveAspectRatio="xMidYMid slice"
-              focusable="false"
-              alt=""
-            />
-            <div className="container">
+            <img src={carouselItem[2].image} aria-hidden="true" alt="" />
+            <div>
               <div className="carousel-caption text-end">
-                <h1>Explore our range of products.</h1>
+                <h1>{carouselItem[2].description}</h1>
               </div>
             </div>
           </a>
