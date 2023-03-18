ea0179a53dd0b217767189b50e2bd7037d912754
Julien Claivaz <julienclaivaz@192.168.1.101>
2023-03-18 12:57:55 +0000

    created cardItem display working

--- src/components/pages/CardItem.js
+++ src/components/pages/CardItem.js
@@ -1,15 +1,23 @@
 import React from "react";
 
+let queryUrl = 'https://dummyjson.com/products/category/mens-shirts'
 
-export default function CardItem() {
+fetch(queryUrl)
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data)
+    })
 
-    <div className="card" style="width: 18rem;">
-  <img className="card-img-top" src="..." alt="Card image cap"/>
+export default function CardItem(props) {
+
+    return(
+    <div className="card" style={{width: "18rem"}}>
+  <img className="card-img-top" src={props.product[0].images[0]} alt="Card image cap"/>
   <div className="card-body">
     <h5 className="card-title">Card title</h5>
     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     <a href="#" className="btn btn-primary">Go somewhere</a>
   </div>
 </div>
-    
+    )
 }
