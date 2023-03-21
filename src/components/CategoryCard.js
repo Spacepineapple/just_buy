19515092e2446602c8fcd0a96ca012bc24d5349d
Osman Dumbuya <dumbuyaosman@icloud.com>
2023-03-21 18:16:07 +0000

    Refactored code for category cards

--- src/components/CategoryCard.js
+++ src/components/CategoryCard.js
@@ -1,11 +1,22 @@
+import { Link } from "react-router-dom";
+
 export default function CategoryCard({ name, products }) {
   // console.log(products);
   return (
-    <a href="https://www.google.com/" className={`category-card ${name}`}>
+    <Link to={`/category/${name}`} className={`category-card`}>
       {/* Display the image for the first product in the products array */}
       <img className="category-card__img" src={products[0]?.image} alt="" />
       {/* Display the name of the category as a caption */}
       <p className="category-card__caption">{name}</p>
-    </a>
+    </Link>
   );
 }
+
+/*
+<a href="https://www.google.com/" className={`category-card ${name}`}>
+  // Display the image for the first product in the products array
+  <img className="category-card__img" src={products[0]?.image} alt="" />
+  // Display the name of the category as a caption
+  <p className="category-card__caption">{name}</p>
+</a>;
+*/
