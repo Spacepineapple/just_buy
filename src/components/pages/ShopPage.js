7857c1b143254fe1f301ef7965e4ac1132ab32f7
Osman Dumbuya <dumbuyaosman@icloud.com>
2023-03-21 16:58:48 +0000

    Refactored category components

--- src/components/pages/ShopPage.js
+++ src/components/pages/ShopPage.js
@@ -1,12 +1,9 @@
-import CategoryCard from "../CategoryCard";
+import Category from "../Category";
 
 export default function ShopPage() {
   return (
     <div className="shop-page">
-      <section class="shop-categories">
-        <h2 class="shop-header__caption">Shop by Categories</h2>
-        <CategoryCard />
-      </section>
+      <Category />
 
       <section class="sales">
         <h2 class="header-caption">Products at Slashed Prices</h2>
@@ -14,34 +11,3 @@ export default function ShopPage() {
     </div>
   );
 }
-
-/**
-function App() {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios(
-        'https://dummyjson.com/products/category/smartphones',
-      );
-      const result2 = await axios(
-        'https://dummyjson.com/products/category/laptops',
-      );
-      setProducts([...result.data, ...result2.data]);
-    };
-    fetchData();
-  }, []);
-
-  return (
-    <div>
-      <h1>Tech Products</h1>
-      {products.map((product) => (
-        <div key={product.id}>
-          <img src={product.image} alt={product.name} />
-          <p>{product.name}</p>
-        </div>
-      ))}
-    </div>
-  );
-}
- */
