d11179d2503d773ab677dfa1cd96903e9f095b9f
Spacepineapple <neal.t.reeves@gmail.com>
2023-03-22 19:12:29 +0000

    Add flex properties and product image to product page

--- src/components/pages/Product.js
+++ src/components/pages/Product.js
@@ -20,11 +20,16 @@ function Product({data}) {
     if (item) {
         return (
             <div className="p-5 mb-4 bg-light rounded-3">
-                <div className="container-fluid py-5">
-                    <h2 className="display-5 fw-bold">{item.name}</h2>
-                    <p className="col-md-8 fs-4">{item.description}</p>
-                    <p className="col-md-8 fs-4">{item.price}</p>
-                    <button type="button" className="btn btn-primary" onClick={console.log("added to cart")}>Add to Cart</button>
+                <div className="d-flex container-fluid col-md-3 py-5">
+                    <div className="col-md-5">
+                        <img src={item.images[0]}/>
+                    </div>
+                    <div className="d-flex col-md-8 flex-column">
+                        <h2 className="display-5 fw-bold">{item.name}</h2>
+                        <p className="col-md-8 fs-4">{item.description}</p>
+                        <p className="col-md-8 fs-4">{item.price}</p>
+                        <button type="button" className="btn btn-primary" onClick={console.log("added to cart")}>Add to Cart</button>
+                    </div>
                 </div>
             </div>
         )}
