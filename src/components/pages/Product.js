ddf7362d2ddba6d3b76212af6c922b11956dd6ef
Spacepineapple <neal.t.reeves@gmail.com>
2023-03-22 20:05:32 +0000

    Add useState hook to ensure product image is never undefined

--- src/components/pages/Product.js
+++ src/components/pages/Product.js
@@ -5,6 +5,7 @@ import {useParams} from "react-router-dom";
 
 function Product({data}) {
     const [item, setItem] = useState([]);
+    const [imgSrc, setImgSrc] = useState();
     let {id} = useParams();
 
     useEffect (() => {
@@ -12,6 +13,7 @@ function Product({data}) {
           .get(`https://dummyjson.com/products/${id}`)
           .then(({ data }) => {
             setItem(data);
+            setImgSrc(data.images[0]);
             console.log(data);
             console.log(id);
           });
@@ -21,10 +23,11 @@ function Product({data}) {
         return (
             <div className="p-5 mb-4 bg-light rounded-3">
                 <div className="d-flex flex-column container-fluid col-md-7 py-5 align-items-center">
+                    <button type="button" className="btn btn-secondary">Back</button>
                     <h2 className="display-5 fw-bold">{item.title}</h2>
                     <div className="product-content d-flex col-md-8">
                         <div className="col-md-5">
-                            <img src={item.images[0]}/>
+                            <img src={imgSrc}/>
                         </div>
                         <div className="d-flex col-md-10 flex-column align-items-center justify-content-center">
                             <p className="col-md-12 fs-4">{item.description}</p>
