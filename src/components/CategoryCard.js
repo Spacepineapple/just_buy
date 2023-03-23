523e6ef24707d059ca296a189c44a8cc6d5c4fd2
Osman Dumbuya <dumbuyaosman@icloud.com>
2023-03-23 19:44:40 +0000

    Add styles to the categories name cards

--- src/components/CategoryCard.js
+++ src/components/CategoryCard.js
@@ -1,22 +1,9 @@
-import { Link, Route, Routes } from "react-router-dom";
-import CategoryProducts from "./pages/CategoryProducts";
-
 export default function CategoryCard({ name, onClick }) {
   // console.log(name);
   return (
     // Render a card with an image and the category name
-    <li
-      style={{
-        display: "inline-block",
-        padding: "10px",
-        border: "1px solid #ccc",
-        borderRadius: "5px",
-        marginRight: "10px",
-        cursor: "pointer",
-      }}
-      onClick={onClick}
-    >
+    <div onClick={onClick} className="category-item">
       {name}
-    </li>
+    </div>
   );
 }
