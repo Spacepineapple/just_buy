dcb07482081401ceee9aa6697de9d94b1f66ee36
Spacepineapple <neal.t.reeves@gmail.com>
2023-03-23 16:44:11 +0000

    Add random quote to thank you page

--- src/components/pages/Thanks.js
+++ src/components/pages/Thanks.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, {useEffect, useState } from "react";
 import store from "../../store";
+import axios from "axios";
+
 
 function ThankYou() {
+    const [quote, setQuote] = useState("Loading");
+
+    useEffect (() => {
+        getData()
+    }, []);
+
+    const getData = () => {
+        axios
+          .get("https://dummyjson.com/quotes/random")
+          .then(({ data }) => {
+            setQuote(data);
+            console.log(data);
+          });
+      };
+    
     localStorage.clear();
     store.dispatch({ type: 'cart/clearCart', payload: "" })
     return(
     <div>
         <h2>Thank you for your purchase</h2>
-        <p>Thank you very much for your purchase. Please look out for an email confirmation which we will send to the email address registered to your account. We very much hope you have enjoyed shopping with us and look forward to seeing you again soon!</p>
+        <p>Thank you very much for your purchase. Please look out for an email confirmation which we will send to the email address registered to your account. We very much hope you have enjoyed shopping with us and look forward to seeing you again soon! We'll leave you with this quote: </p>
+        <br/>
+        <p>"{`${quote.quote}`}" - <em>{`${quote.author}`}</em></p>
     </div>)
 }
 
