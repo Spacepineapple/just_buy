df15a556560cf6b2887d7116011c167152751138
Spacepineapple <neal.t.reeves@gmail.com>
2023-04-03 11:54:02 +0100

    Convert basket cards from bootstrap and update alignment and layout of basket card

--- src/components/BasketCard.js
+++ src/components/BasketCard.js
@@ -18,30 +18,36 @@ function reduceItem(id) {
 }
 
 function changeItem(event) {
-    let payload = {id: event.target.name, value: event.target.value}
-    store.dispatch({ type: "cart/productChanged", payload: payload})
+    if (event.target.value<0) {
+        alert("Please input a valid positive number or use the + and - buttons provided");
+    }
+    else {
+        let payload = {id: event.target.name, value: Number(event.target.value)}
+        store.dispatch({ type: "cart/productChanged", payload: payload})    
+    }
 }
 
 function BasketCard({props}) {
     return (
-        <div className="card mb-3" styles="max-width: 540px;">
-        <div className="row g-0">
-            <div className="col-md-4">
+        <div className="basket-card">
+        <div className="basket-content">
+            <div className="basket-image">
             {<img src={props.product.images[0]} className="img-fluid rounded-start" alt={props.description}/>}
             </div>
-            <div className="col-md-8">
-            <div className="card-body">
+            <div className="basket-item">
                 <h5 className="card-title">{props.product.title}</h5>
                 <p className="card-text">{props.product.description}</p>
-                <p className="card-text">{props.product.price}</p>
-                <button onClick={()=>removeItem(props.product.id)}>Remove</button>
-                <br/>
+                <p className="card-text">£{props.product.price}</p>
+            </div>
+            <div className="basket-buttons">
                 <div className="buttons-div">
                 <button onClick={()=>increaseItem(props.product.id)}>+</button>
-                <input type="number" id="product-count" name={props.product.id} value={props.count} placeHolder={props.count} onChange={changeItem}/>
+                <input type="number" id="product-count" name={props.product.id} value={props.count} placeHolder={props.count} onChange={changeItem} onclick={() =>{this.select()}} />
                 <button onClick={()=>reduceItem(props.product.id)}>-</button>
                 </div>
             </div>
+            <div className="remove">
+                <button onClick={()=>removeItem(props.product.id)}>Remove</button>
             </div>
         </div>
         </div>
